5accbcfbe0d60bf4ba0f56a4ef4447d6665435eb
abh628 <rockybhaiowner@gmail.com>
2026-01-02 13:16:46 +0530

    Update play.py

--- SaregamaMusic/utils/inline/play.py
+++ SaregamaMusic/utils/inline/play.py
@@ -60,7 +60,9 @@ def stream_markup_timer(_, chat_id, played, dur):
             InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
-    ]
+      
+         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
+        ]
     return buttons
 
 
@@ -73,7 +75,9 @@ def stream_markup(_, chat_id):
             InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
-    ]
+      
+         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
+        ]
     return buttons
 
 
